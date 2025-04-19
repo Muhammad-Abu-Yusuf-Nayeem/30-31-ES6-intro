4dbcb0332ec311631a9f6b7a45d98c0aaa0cf1fb
assignment-01 <ykhan9463@gmail.com>
2025-04-19 23:46:39 +0600

    Getter and Setter Methods

--- class.js
+++ class.js
@@ -17,21 +17,45 @@ class Person {
 const person1 = new Person("Alice", 25);
 person1.greet(); // Output: Hello, my name is Alice and I'm 25 years old.
 
-
-
 // Define a subclass named Student that extends Person
 class Student extends Person {
-    constructor(name, age, grade) {
-      super(name, age); // call the parent class constructor
-      this.grade = grade;
-    }
-  
-    study() {
-      console.log(`${this.name} is studying in grade ${this.grade}.`);
+  constructor(name, age, grade) {
+    super(name, age); // call the parent class constructor
+    this.grade = grade;
+  }
+
+  study() {
+    console.log(`${this.name} is studying in grade ${this.grade}.`);
+  }
+}
+
+const student1 = new Student("Bob", 17, 10);
+student1.greet(); // From parent class
+student1.study(); // From child class
+
+class Product {
+  constructor(name, price) {
+    this._name = name;
+    this._price = price;
+  }
+
+  // getter
+  get price() {
+    return this._price;
+  }
+
+  // setter
+  set price(newPrice) {
+    if (newPrice > 0) {
+      this._price = newPrice;
+    } else {
+      console.log("Price must be positive!");
     }
   }
-  
-  const student1 = new Student("Bob", 17, 10);
-  student1.greet();   // From parent class
-  student1.study();   // From child class
-  
+}
+
+const item = new Product("Laptop", 1000);
+console.log(item.price); // 1000
+item.price = 1200;
+console.log(item.price); // 1200
+item.price = -5; // Price must be positive!
