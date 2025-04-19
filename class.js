0ba50e4c683dd66976ea3eee27fbc210788ee0de
assignment-01 <ykhan9463@gmail.com>
2025-04-19 23:46:09 +0600

    Inheritance (Extending a Class)

--- class.js
+++ class.js
@@ -16,3 +16,22 @@ class Person {
 // Create an object from the class
 const person1 = new Person("Alice", 25);
 person1.greet(); // Output: Hello, my name is Alice and I'm 25 years old.
+
+
+
+// Define a subclass named Student that extends Person
+class Student extends Person {
+    constructor(name, age, grade) {
+      super(name, age); // call the parent class constructor
+      this.grade = grade;
+    }
+  
+    study() {
+      console.log(`${this.name} is studying in grade ${this.grade}.`);
+    }
+  }
+  
+  const student1 = new Student("Bob", 17, 10);
+  student1.greet();   // From parent class
+  student1.study();   // From child class
+  
